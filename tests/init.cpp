4195a92fc8fb641d7eefcd40ca56a3575bf07954
BURNINGTIGER <Aleks203467@gmail.com>
2017-12-19 03:14:41 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,6 +5,6 @@ SCENARIO("compute", "[compute]") {
 	dicstra init(std::ifstream("f1.txt"));
 	double hold;
 	init.compute();
-	hold = init.show()
+	hold = init.show();
 	REQUIRE(hold == [16.5714]);
 }
