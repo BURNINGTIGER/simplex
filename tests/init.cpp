523452b0c311b95dc092a551afb0ffc300c14072
BURNINGTIGER <Aleks203467@gmail.com>
2017-12-19 04:51:32 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,9 +1,20 @@
 #include <simplex.hpp>
 #include <catch.hpp>
-
+#include <iostream>
+#include <fstream>
 
 SCENARIO("compute", "[compute]") {
-	simplex init(std::ifstream("f1.txt"));
+	std::ofstream fout("f2.txt");
+	fout << "5" << std::endl;
+	fout << "5" << std::endl;
+	fout << "0" << std::endl;
+	fout << "0 0 1 2 3" << std::endl;
+	fout << "4 4 2 1 1" << std::endl;
+	fout << "5 3 1 4 0" << std::endl;
+	fout << "6 6 0 0.5  1" << std::endl;
+	fout << "0 0 -8 -6 -2" ;
+	fout.close();
+	simplex init(std::ifstream("f2.txt"));
 	double hold;
 	init.compute();
 	hold = init.show();
